694d67bdea7fac64b36e6910513f81400d179edf
Antoine Rouaze <antoine.rouaze@zenika.com>
2012-08-29 14:17:45 +0200

    Implement the new binary adapter in Ivy component.

--- cudf-adapter-ivy/src/main/java/com/zenika/cudf/apdater/IvyDescriptorAdapter.java
+++ cudf-adapter-ivy/src/main/java/com/zenika/cudf/apdater/IvyDescriptorAdapter.java
@@ -1,17 +1,8 @@
 package com.zenika.cudf.apdater;
 
-import com.zenika.cudf.adapter.Adapter;
-import com.zenika.cudf.model.Binary;
-import com.zenika.cudf.model.BinaryId;
-import com.zenika.cudf.model.CUDFDescriptor;
-import org.apache.ivy.core.module.descriptor.Artifact;
-import org.apache.ivy.core.module.descriptor.Configuration;
-import org.apache.ivy.core.module.descriptor.DefaultArtifact;
-import org.apache.ivy.core.module.descriptor.DefaultDependencyArtifactDescriptor;
-import org.apache.ivy.core.module.descriptor.DefaultDependencyDescriptor;
-import org.apache.ivy.core.module.descriptor.DefaultModuleDescriptor;
-import org.apache.ivy.core.module.descriptor.DependencyDescriptor;
-import org.apache.ivy.core.module.descriptor.ModuleDescriptor;
+import com.zenika.cudf.adapter.DescriptorAdapter;
+import com.zenika.cudf.model.*;
+import org.apache.ivy.core.module.descriptor.*;
 import org.apache.ivy.core.module.id.ModuleRevisionId;
 
 import java.util.Date;
@@ -21,11 +12,12 @@ import java.util.Set;
 /**
  * @author Antoine Rouaze <antoine.rouaze@zenika.com>
  */
-public class IvyAdapter implements Adapter<Set<ModuleDescriptor>, Set<DependencyDescriptor>> {
+public class IvyDescriptorAdapter
+        implements DescriptorAdapter<Set<ModuleDescriptor>, Set<DependencyDescriptor>> {
 
     @Override
     public Set<ModuleDescriptor> fromCUDF(CUDFDescriptor descriptor) {
-        Set<Binary> binaries = descriptor.getPackages();
+        Set<Binary> binaries = descriptor.getBinaries().getAllBinaries();
         Set<ModuleDescriptor> moduleDescriptors = new HashSet<ModuleDescriptor>();
         for (Binary binary : binaries) {
             BinaryId binaryId = binary.getBinaryId();
@@ -65,17 +57,17 @@ public class IvyAdapter implements Adapter<Set<ModuleDescriptor>, Set<Dependency
 
     @Override
     public CUDFDescriptor toCUDF(Set<DependencyDescriptor> descriptors) {
-        Set<Binary> binaries = new HashSet<Binary>();
+        Binaries binaries = new DefaultBinaries();
         for (DependencyDescriptor dependencyDescriptor : descriptors) {
             ModuleRevisionId moduleRevisionId = dependencyDescriptor.getDependencyRevisionId();
             //TODO: Find best way to flag unknown CUDF version
             BinaryId binaryId = new BinaryId(moduleRevisionId.getName(), moduleRevisionId.getOrganisation(), 0);
             Binary binary = new Binary(binaryId);
             binary.setRevision(moduleRevisionId.getRevision());
-            binaries.add(binary);
+            binaries.addBinary(binary);
         }
         CUDFDescriptor descriptor = new CUDFDescriptor();
-        descriptor.setPackages(binaries);
+        descriptor.setBinaries(binaries);
         return descriptor;
     }
 
